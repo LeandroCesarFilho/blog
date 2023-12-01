f8e3cdcdd9907cce6c60bce9855a8d1b2c97e9b6
Leandro Filho <leandro.filho@sou.unifal-mg.edu.br>
2023-12-01 15:12:11 -0300

    feat: Adition of anotattion PutMapping

--- src/main/java/br/com/unifalmg/blog/controller/BlogController.java
+++ src/main/java/br/com/unifalmg/blog/controller/BlogController.java
@@ -6,10 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -60,7 +57,7 @@ public class BlogController {
         return "editUser";
     }
 
-    @PostMapping("/edituser/{id}")
+    @PutMapping("/edituser/{id}")
     public String editUser(@ModelAttribute("user") User user) {
         log.info("Usuário alterado");
         User editedUser = service.edit(user.getId(),user);
